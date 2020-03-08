c4373d51e2c92241fb422c4c3d69e9d8acedbee9
Alex Clay <alexclay@me.com>
2020-03-08 15:24:35 +0000

    domestic sales

---
+++ src/back-end/models/DomesticSales.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//TODO
+// Add id in??
+const  DSalesSchema = new Schema({
+    ticketNumber: {
+        type: int,
+        required: true
+    },
+    fare: {
+        type: int,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    USDExchangeRate: {
+        type: int,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    commissionRate: {
+        type: int,
+        required: true
+    },
+    advisorCode: {
+        type: int,
+        required: true
+    },
+    salePeriodStart:{
+        type:Date,
+        required: true
+    },
+    salePeriodEnd:{
+        type:Date,
+        required: true
+    },
+    notes:{
+        type:String,
+    }
+
+});
+
+module.exports = DSales= mongoose.model('DSales', DSalesSchema);
