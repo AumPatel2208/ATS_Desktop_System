8bf399271a40957b907d7eb3ff50ef787c48d6c1
Alex Clay <alexclay@me.com>
2020-04-09 21:55:58 +0100

    overhauling individual reports

--- src/Components/ReportTableI.js
+++ src/Components/ReportTableI.js
@@ -23,6 +23,7 @@ export default class ReportTableI extends Component {
     constructor(props) {
         super(props);
         this.toPDF =this.toPDF.bind(this);
+        this.toPDFB =this.toPDFB.bind(this);
 
 
     //Set the state to an empty list of objects that will be taken from the database
@@ -86,23 +87,49 @@ export default class ReportTableI extends Component {
             this.setState({sType: "OM"})
         }
     }
-
-    roleHandler(){
-      if (this.state.setType !== "OM"){
-            return <Fragment>
-                <h3>Showing Sales For Advisor {this.state.code} </h3>
-            </Fragment>
+    cashCheck(paymentMethod, fare){
+        if (paymentMethod === "Cash"){
+            return fare;
         }
         else {
+            return 0;
+        }
+    }
+
+    creditCheck(paymentMethod, fare){
+       if (paymentMethod === "Credit"){
+            return fare;
+        }else {
+           return 0;
+       }
+    }
+
+    commissionCheck9(commission, fare){
+        if (commission === "9"){
+            return fare;
+        }else {
+            return 0;
+        }
+    }
+    commissionCheck5(commission, fare){
+        if (commission === "5"){
+            return fare;
+        }else {
+            return 0;
+        }
+    }
+
+    roleHandler(){
+      if (this.state.setType == "OM"){
             return <Fragment>
                 <FormLabel>Enter Advisor Code</FormLabel>
                 <FormControl
                     autoFocus
                     type="string"
-                    value={this.state.sales.inputCode}
+                    value={this.state.sales.code}
                     onChange={e => {
                         this.setState({
-                            inputCode: e.target.value
+                            code: e.target.value
                         });
                     }}
                 />
@@ -111,14 +138,72 @@ export default class ReportTableI extends Component {
         }
     }
 
+    aggregate(value) {
+        let x = 0;
+        if (value === 1) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                x += parseInt(this.state.sales[i].fare);
+            }
+            return x;
+        } else if (value === 2) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                let y = parseInt(this.state.sales[i].fare) * parseInt(this.state.sales[i].USDExchangeRate);
+                x += y;
+            }
+            return x;
+        } else if (value === 3) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                if (this.state.sales[i].paymentMethod === "Cash") {
+                    x += parseInt(this.state.sales[i].fare);
+                }
+            }
+            return x;
+        } else if (value === 4) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                if (this.state.sales[i].paymentMethod === "Credit") {
+                    x += parseInt(this.state.sales[i].fare);
+                }
+
+            }
+            return x;
+        } else if (value === 5) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                x += parseInt(this.state.sales[i].localTax);
+            }
+            return x;
+        } else if (value === 6) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                if (this.state.sales[i].commissionRate === 9) {
+                    x += parseInt(this.state.sales[i].fare);
+                }
+            }
+            return x;
+        } else if (value === 7) {
+            for (var i = 0; i < this.state.sales.length; i++) {
+                if (this.state.sales[i].commissionRate === 5) {
+                    x += parseInt(this.state.sales[i].fare);
+                }
+            }
+            return x;
+        }
+        return x;
+    }
 
     //to get the document into a pdf
     toPDF() {
         var pdf = new jsPDF('l', 'pt', 'A4');
         pdf.setFontSize(10);
+        pdf.text( "For Advisor: " +this.state.code, 50, 20);
         var source = document.getElementById("export");
         pdf.autoTable({html: '#export'});
-        pdf.save("individualReport.pdf")
+        pdf.save("DomesticIndividualReport.pdf")
+    }
+
+    toPDFB() {
+        var pdf = new jsPDF('l', 'pt', 'A4');
+        pdf.setFontSize(9);
+        pdf.autoTable({html: '#exportB'});
+        pdf.save("InterlineIndividualReport.pdf")
     }
 
     onOpenClick(e, _id) {
@@ -165,49 +250,8 @@ export default class ReportTableI extends Component {
         return (
             <Container>
                 <Form>
+                    <h2>For Advisor: {this.state.code}</h2>
                     <FormGroup controlId="saleT" bssize="large">
-                        <Dropdown
-                            onSelect={key => {
-                                this.setState({ saleTypeValue: key });
-
-                                if (key === 'Interline') {
-                                    this.setState({
-                                        sales: this.state.sales.filter(
-                                            sale =>
-                                                String(
-                                                    sale[this.state.saleT]
-                                                ) === 'Interline'
-                                        )
-                                    });
-                                } else {
-                                    this.setState({
-                                        sales: this.state.sales.filter(
-                                            sale =>
-                                                String(
-                                                    sale[this.state.saleT]
-                                                ) === 'Domestic'
-                                        )
-                                    });
-                                }
-                            }}
-                        >
-                            <Dropdown.Toggle
-                                variant="success"
-                                id="dropdown-basic"
-                            >
-                                {_.startCase(this.state.saleTypeValue)}
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item eventKey="Domestic">
-                                    Domestic
-                                </Dropdown.Item>
-                                <Dropdown.Item eventKey="Interline">
-                                    Interline
-                                </Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        <br></br>
                         <FormLabel>From: </FormLabel>
                         <DatePicker
                             selected={this.state.startDate}
@@ -252,80 +296,174 @@ export default class ReportTableI extends Component {
                                         this.setState({ sales });
                                     });
 
-                                this.setState({
-                                    sales: this.state.sales.filter(
-                                        sale =>
-                                            String(sale[this.state.code]) ===
-                                            String(this.state.inputCode)
-                                    )
-                                });
+                                const dl = this.state.sales.filter(
+                                    i => i.advisorCode === this.state.code);
+                                this.setState({sales: dl});
                             }}
                             block
                         >
                             Filter Report
                         </Button>
 
+
+
+                        <h2>Domestic Sales Report</h2>
                     </FormGroup>
                 </Form>
                 <button onClick={this.toPDF}>Download PDF</button>
 
-
-
                 <Table grid className="mt-4" id="export">
                     <thead>
                         <tr>
-                            <th>Advisor Code</th>
                             <th>Ticket Number</th>
-                            <th>Fare</th>
-                            <th>Currency</th>
-                            <th>Payment Method</th>
-                            <th>Exchange Rate</th>
-                            <th>Commission Rate</th>
-                            <th>Card Number</th>
-                            <th>Expiration Date</th>
-                            <th>Security Code</th>
-                            <th>Sale Date</th>
+                            <th>Fare(Local)</th>
+                            <th>Fare(USD)</th>
+                            <th>Cash</th>
+                            <th>Credit Card(USD)</th>
+                            <th>Credit Card(local)</th>
+                            <th>Taxes</th>
+                            <th>Total Paid(local)</th>
+                            <th>Commission 9%</th>
+                            <th>Commission 5%</th>
                             <th>Notes</th>
                         </tr>
                     </thead>
+
                     <tbody>
-                        {this.state.sales.map(
-                            ({
-                                _id,
-                                advisorCode,
-                                ticketNumber,
-                                fare,
-                                currency,
-                                paymentMethod,
-                                USDExchangeRate,
-                                commissionRate,
-                                creditCardNum,
-                                expDate,
-                                securityCode,
-                                saleDate,
-                                notes
-                            }) => (
-                                <Fragment key={_id}>
-                                    {row(
-                                        _id,
-                                        advisorCode,
-                                        ticketNumber,
-                                        fare,
-                                        currency,
-                                        paymentMethod,
-                                        USDExchangeRate,
-                                        commissionRate,
-                                        creditCardNum,
-                                        expDate,
-                                        securityCode,
-                                        saleDate.substring(0,10),
-                                        notes
-                                    )}
-                                </Fragment>
-                            )
-                        )}
+                    {this.state.sales.map(({
+
+                                                ticketNumber,
+                                                fare,
+                                                USDExchangeRate,
+                                                localTax,
+                                                paymentMethod,
+                                                commissionRate,
+                                                creditCardNum,
+                                                expDate,
+                                                securityCode,
+                                                saleDate,
+                                                notes,
+                                                saleType
+                                            }) => {
+                        return (
+                            <tr >
+                                <td>{ticketNumber}</td>
+                                <td> {fare}</td>
+                                <td>{(fare * USDExchangeRate).toFixed(3)}</td>
+                                <td> {this.cashCheck(paymentMethod, fare)}</td>
+                                <td> {this.creditCheck(paymentMethod, fare)}</td>
+                                <td> {this.creditCheck(paymentMethod, fare) * USDExchangeRate}</td>
+                                <td> {localTax}</td>
+                                <td> {localTax + fare}</td>
+                                <td> {this.commissionCheck9(commissionRate, fare)}</td>
+                                <td> {this.commissionCheck5(commissionRate, fare)}</td>
+                                <td> {notes}</td>
+
+                            </tr>
+                        );
+                    })}
                     </tbody>
                 </Table>
+                <Table grid className="mt-4" id="export2">
+                    <thead>
+                    <tr>
+                        <th>Ticket Total</th>
+                        <th>Total Fare(Local)</th>
+                        <th>Total Fare(USD)</th>
+                        <th>Total Cash</th>
+                        <th>Total Credit Card(USD)</th>
+                        <th>Total Credit Card(local)</th>
+                        <th>Total Taxes</th>
+                        <th>Total Paid(local)</th>
+                        <th>Total Commission 9%</th>
+                        <th>Total Commission 5%</th>
+                        <th>Total Commission Amounts</th>
+                        <th>Net Amounts For Debit</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                            <tr >
+                                <td>{this.state.sales.length}</td>
+                                <td> {this.aggregate(1)}</td>
+                                <td> {this.aggregate(2)}</td>
+                                <td> {this.aggregate(3)}</td>
+                                <td> {this.aggregate(4)}</td>
+                                <td> "fix this one"</td>
+                                <td> {this.aggregate(5)}</td>
+                                <td> {this.aggregate(5) + this.aggregate(1)}</td>
+                                <td> {this.aggregate(6)}</td>
+                                <td> {this.aggregate(7)}</td>
+                               <td>{(this.aggregate(6)*.09)+(this.aggregate(7)*.05)}</td>
+                                <td> {(this.aggregate(6)+this.aggregate(7))-((this.aggregate(6)*.09)+(this.aggregate(7)*.05))}</td>
+                            </tr>
+                    </tbody>
+                </Table>
+
+
+
+                <h2>Interline Sales Report</h2>
+                <button onClick={this.toPDFB}>Download PDF</button>
+                <Table grid className="mt-4" id="exportB">
+                    <thead>
+                    <tr>
+                        <th>Advisor Code</th>
+                        <th>Ticket Number</th>
+                        <th>Fare</th>
+                        <th>Currency</th>
+                        <th>Payment Method</th>
+                        <th>Exchange Rate</th>
+                        <th>Commission Rate</th>
+                        <th>Card Number</th>
+                        <th>Expiration Date</th>
+                        <th>Security Code</th>
+                        <th>Sale Date</th>
+                        <th>Notes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.sales.map(
+                        ({
+                             _id,
+                             advisorCode,
+                             ticketNumber,
+                             fare,
+                             currency,
+                             paymentMethod,
+                             USDExchangeRate,
+                             commissionRate,
+                             creditCardNum,
+                             expDate,
+                             securityCode,
+                             saleDate,
+                             notes
+                         }) => (
+                            <Fragment key={_id}>
+                                {row(
+                                    _id,
+                                    advisorCode,
+                                    ticketNumber,
+                                    fare,
+                                    currency,
+                                    paymentMethod,
+                                    USDExchangeRate,
+                                    commissionRate,
+                                    creditCardNum,
+                                    expDate,
+                                    securityCode,
+                                    saleDate.substring(0,10),
+                                    notes
+                                )}
+                            </Fragment>
+                        )
+                    )}
+                    </tbody>
+                </Table>
+
+
+
+
+
             </Container>
 
         );
