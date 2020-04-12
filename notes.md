9405feca55a37646a3a45f713c70972c95fb35af
Aum <imaum2208@gmail.com>
2020-04-12 11:14:16 +0100

    Post presentation aum

--- notes.md
+++ notes.md
@@ -1,23 +1,32 @@
+# TODO
+## - [ ] Database Transactions / Concurrency control 
+## - [ ] GUI 
+## - [ ] Efficient/extra features 
+## - [ ] Code Cleanup
+
 # Recording Notes
 
 ## Sales
 -  **2.1**
-   - [ ] **2.1.3**. Make an interline sale to a valued customer, DaveD. Record the sale as “pay later” (see 2.1.4). Use blank 444-0000-0025 and sale details like the first sale by Penelope. (see the test data given in the Sample Data document, p.2). 
+   - [x] **2.1.3**. Make an interline sale to a valued customer, DaveD. Record the sale as “pay later” (see 2.1.4). Use blank 444-0000-0025 and sale details like the first sale by Penelope. (see the test data given in the Sample Data document, p.2). 
+     - data:Local currency: **220.00** Taxes:Local: **23.00**,Other: **35.00**
       - Correct behaviour: The sale made successfully and DaveD’s balance increased correctly. 1 mark for correct behaviour. 
-    - [ ] **2.1.4**. DaveD pays for the last ticket purchased. 
+    - [x] **2.1.4**. DaveD pays for the last ticket purchased. 
       - Correct behaviour: DaveD’s balance restored to the previous level. 1 mark for correct behaviour. 
+    - [x] **2.1.5.** Repeat the above for domestic sales (use blanks 201-0000-0012 for cash and 201-0000-0013 for credit card) and make up the necessary sale details. 
+       - Correct behaviour: The sales made successfully. 4 marks for correct behaviour. 
 - **2.2.** Refund a ticket 
-  - [ ] **2.2.1.** Refund 444-0000-0003
+  - [x] **2.2.1.** Refund 444-0000-0003
     -  Correct behaviour: Data entered and saved correctly in a log file, not DB. 2 marks for correct behaviour. 
 
 ## Customers
-- [ ] **4.1**  Show Records of available customers. 
+- [x] **4.1**  Show Records of available customers. 
   - Correct behaviour: Records available and as defined in the test data given in the Sample Data document (SarahB, etc.), or some equivalent records exist. 2 marks for correct behaviour. 
-- [ ] **4.2.** Alert of late payment. The records about late payment by Chris. 
+- [x] **4.2.** Alert of late payment. The records about late payment by Chris. 
   - Correct behaviour: Late payment discovered, and alert raised for the Manager; might need to change the system clock (this is possible, since you will use your own computer, which will be the case). Alternatively, change the date of the unpaid sale, in order to initiate the functionally of raising, and detecting, late payment. Also, the Manager acknowledges the alert, e.g. closes the pop-up message, or does an equivalent action. **5 marks** for correct behaviour. (If no acknowledgment or equivalent functionality exists, only **2 marks**). 
 
 ## General Functionality
-- [ ]  **5.3.** Multiple tables’ search (e.g. Amount due by a Regular/Valued customer is likely to be one such, or any other). 
+- [x]  **5.3.** Multiple tables’ search (e.g. Amount due by a Regular/Valued customer is likely to be one such, or any other). 
   - Correct behaviour: Data retrieved from multiple tables (e.g. SQL JOIN used, etc.). 1 mark. 
 - **5.4.** Backup/Restore. Login as an Administrator. 
   - [x] **5.4.1.** Show Backup functionality. Correct behaviour: Backup successfully completed; separate (set of) 
@@ -50,8 +59,15 @@ appearance, colour scheme).
 
 
 
-
-
+### Usernames and passwords:
+- penelope
+  - PinkMobile
+- dennis
+  - Gnasher
+- minnie
+  - NotiGirl
+- arthur
+  - LiesaLot
 
 
 
--- src/Components/SaleEditor.js
+++ src/Components/SaleEditor.js
@@ -50,42 +50,43 @@ export default class SaleEditor extends Component {
     }
     render() {
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <FormGroup controlId="currency" bssize="large">
-                    <FormLabel>currency</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="currency"
-                        value={this.state.sale.currency}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.currency = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <FormGroup controlId="USDExchangeRate" bssize="large">
-                    <FormLabel>USDExchangeRate</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="USDExchangeRate"
-                        value={this.state.sale.USDExchangeRate}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.USDExchangeRate = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <FormGroup controlId="paymentMethod" bssize="large">
-                    <FormLabel>paymentMethod</FormLabel>
-                    {/* <FormControl
+            <Container>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="currency" bssize="large">
+                        <FormLabel>currency</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="currency"
+                            value={this.state.sale.currency}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.currency = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="USDExchangeRate" bssize="large">
+                        <FormLabel>USDExchangeRate</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="USDExchangeRate"
+                            value={this.state.sale.USDExchangeRate}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.USDExchangeRate = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="paymentMethod" bssize="large">
+                        <FormLabel>paymentMethod</FormLabel>
+                        {/* <FormControl
                         autoFocus
                         type="paymentMethod"
                         value={this.state.sale.paymentMethod}
@@ -98,96 +99,102 @@ export default class SaleEditor extends Component {
                             });
                         }}
                     /> */}
-                    <Dropdown
-                        onSelect={(key) => {
-                            this.setState({
-                                sale: {
-                                    ...this.state.sale,
-                                    paymentMethod: key,
-                                },
-                            });
-                        }}
-                    >
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            {this.state.sale.paymentMethod}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item eventKey="Cash">Cash</Dropdown.Item>
-                            <Dropdown.Item eventKey="CreditCard">
-                                Credit card
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </FormGroup>
+                        <Dropdown
+                            onSelect={(key) => {
+                                this.setState({
+                                    sale: {
+                                        ...this.state.sale,
+                                        paymentMethod: key,
+                                    },
+                                });
+                            }}
+                        >
+                            <Dropdown.Toggle
+                                variant="success"
+                                id="dropdown-basic"
+                            >
+                                {this.state.sale.paymentMethod}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item eventKey="Cash">
+                                    Cash
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey="CreditCard">
+                                    Credit card
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </FormGroup>
 
-                <FormGroup controlId="creditCardNum" bssize="large">
-                    <FormLabel>creditCardNum</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="creditCardNum"
-                        value={this.state.sale.creditCardNum}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.creditCardNum = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <FormGroup controlId="expDate" bssize="large">
-                    <FormLabel>expDate</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="date"
-                        value={this.state.sale.expDate}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.expDate = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <FormGroup controlId="securityCode" bssize="large">
-                    <FormLabel>securityCode</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="securityCode"
-                        value={this.state.sale.securityCode}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.securityCode = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <FormGroup controlId="paymentDate" bssize="large">
-                    <FormLabel>paymentDate</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="date"
-                        value={this.state.sale.paymentDate}
-                        // onChange={e => this.setState(e.target.value)}
-                        onChange={(e) => {
-                            var tempSale = this.state.sale;
-                            tempSale.paymentDate = e.target.value;
-                            this.setState({
-                                sale: tempSale,
-                            });
-                        }}
-                    />
-                </FormGroup>
-                <Button block bssize="large" type="submit">
-                    'PAY'
-                </Button>
-            </Form>
+                    <FormGroup controlId="creditCardNum" bssize="large">
+                        <FormLabel>creditCardNum</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="creditCardNum"
+                            value={this.state.sale.creditCardNum}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.creditCardNum = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="expDate" bssize="large">
+                        <FormLabel>expDate</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="date"
+                            value={this.state.sale.expDate}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.expDate = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="securityCode" bssize="large">
+                        <FormLabel>securityCode</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="securityCode"
+                            value={this.state.sale.securityCode}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.securityCode = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="paymentDate" bssize="large">
+                        <FormLabel>paymentDate</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="date"
+                            value={this.state.sale.paymentDate}
+                            // onChange={e => this.setState(e.target.value)}
+                            onChange={(e) => {
+                                var tempSale = this.state.sale;
+                                tempSale.paymentDate = e.target.value;
+                                this.setState({
+                                    sale: tempSale,
+                                });
+                            }}
+                        />
+                    </FormGroup>
+                    <Button block bssize="large" type="submit">
+                        'PAY'
+                    </Button>
+                </Form>
+            </Container>
         );
     }
 }
